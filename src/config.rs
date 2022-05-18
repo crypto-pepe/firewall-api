ba7ae00708f4dbd96d71bb08854005e57e1aad90
hehegrrrhehegrr <ilwhmcwo@tuta.io>
2022-05-18 17:26:07 +0300

    Use only pepe_config

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,3 @@
-use std::env;
-use std::fs::File;
-use std::io::Read;
-
 use config::ConfigError;
 use serde::{Deserialize, Serialize};
 
@@ -19,17 +15,6 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Result<Self, ConfigError> {
-        match env::var("CONFIG_PATH") {
-            Ok(cfg_path) => Config::from_file(cfg_path.as_str()),
-            Err(_) => pepe_config::load(DEFAULT_CONFIG, config::FileFormat::Yaml),
-        }
-    }
-
-    pub fn from_file(filename: &str) -> Result<Self, ConfigError> {
-        let mut file = File::open(filename).map_err(|e| ConfigError::Message(e.to_string()))?;
-        let mut data = String::new();
-        file.read_to_string(&mut data)
-            .map_err(|e| ConfigError::Message(e.to_string()))?;
-        pepe_config::load(&*data, config::FileFormat::Yaml)
+        pepe_config::load(DEFAULT_CONFIG, config::FileFormat::Yaml)
     }
 }
