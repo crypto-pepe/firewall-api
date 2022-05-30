f7519f7e1a4e29ace293a5918ad8a94302d8923f
hehegrrrhehegrr <ilwhmcwo@tuta.io>
2022-05-30 12:23:17 +0300

    Move From trait for error to api

--- src/unbanner/mod.rs
+++ src/unbanner/mod.rs
@@ -28,20 +28,3 @@ pub enum UnbanStatus {
     // Exec name, error
     Error(String, String),
 }
-
-impl From<Vec<UnbanStatus>> for ErrorResponse {
-    fn from(uss: Vec<UnbanStatus>) -> Self {
-        let mut desc = HashMap::new();
-        for us in uss {
-            match us {
-                UnbanStatus::Ok(s) => desc.insert(s, "OK".to_string()),
-                UnbanStatus::Error(s, c) => desc.insert(s, c),
-            };
-        }
-        ErrorResponse {
-            code: 500,
-            reason: "Some executors didn't unban successfully".to_string(),
-            details: Some(desc),
-        }
-    }
-}
